e4e1eda12d9997c0b83b64f4e12850e7d7f08afb
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-22 02:49:40 +0900

    new、createアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'posts#index'
-  resources :posts, only: [:new]
-  
+  resources :posts, only: [:new, :create]
+
 end
