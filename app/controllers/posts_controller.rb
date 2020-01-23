ea64397f1ed8994b9dfba36df00b934090a3f50e
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-23 13:42:39 +0900

    バリデート設定　他

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,20 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to root_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
 
   private
   def post_params
     params.require(:post).permit(:title, :content)
   end
-  
+
 end
