2b1e0a0efc5b71d75d93ab2030b0ede36d68d51e
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-24 19:47:26 +0900

    ユーザーID追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_071909) do
+ActiveRecord::Schema.define(version: 2020_01_24_100620) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
